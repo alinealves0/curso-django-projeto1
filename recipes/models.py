34840e289cb14e6a49c6f8f6e6cb09ac5d9b9213
Aline Alves Gonçalves <alinealves0@hotmail.com>
2024-01-03 11:08:19 -0300

    Modificações em settings, urls, admin, models na pasta projeto

--- recipes/models.py
+++ recipes/models.py
@@ -1,10 +1,14 @@
 from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=65)
-
+    
+    def __str__(self):
+        return self.name
+    
 class Recipe(models.Model):
     title = models.CharField(max_length=65)
     description = models.CharField(max_length=165)
@@ -26,3 +30,7 @@ class Recipe(models.Model):
     author = models.ForeignKey(
         User, on_delete=models.SET_NULL, null=True
     )
+
+
+    def __str__(self):
+        return self.title
