47056c0fed0830c3bc9a6fbc7b4eb0b315ac6172
Aline Alves Gonçalves <alinealves0@hotmail.com>
2024-01-03 18:03:12 -0300

    Modificações em models e views.py em recipes e category.html e recipe.html

--- recipes/views.py
+++ recipes/views.py
@@ -1,6 +1,7 @@
+from django.http import Http404
 from django.shortcuts import render
 from utils.recipes.factory import make_recipe
-from .models import Recipe
+from recipes.models import Recipe
 
 
 def home(request):
@@ -17,6 +18,9 @@ def category(request, category_id):
         category__id=category_id,
         is_published=True,
     ).order_by('-id')
+    if not recipes:
+        raise Http404('Not found 🥲')
+    
     return render(request, 'recipes/pages/category.html', context={
         'recipes': recipes,
     })
