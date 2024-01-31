2c38ad9e8c97810f6bf62d3546941926c7aa9bef
Aline Alves Gonçalves <alinealves0@hotmail.com>
2024-01-31 15:15:51 -0300

    Modificações em recipes/views.py

--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,6 @@
-from django.http.response import Http404
-from django.shortcuts import get_list_or_404, get_object_or_404, render
+from django.db.models import Q #type:ignore
+from django.http.response import Http404 #type:ignore
+from django.shortcuts import get_list_or_404, get_object_or_404, render #type:ignore
 from recipes.models import Recipe
 
 
@@ -36,7 +37,14 @@ def search(request):
 
     if not search_term:
         raise Http404()
+    
+    recipes = Recipe.objects.filter(
+        Q(title__icontains=search_term) |
+        Q(description__icontains=search_term),
+    ).order_by('-id')
+
     return render(request, 'recipes/pages/search.html', {
         'page_title': f'Search for "{search_term}" |',
         'search_term': search_term,
+        'recipes': recipes,
     })
