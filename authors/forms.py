c9dd06e62c8ff93d209ef21a87e813b5e70bed01
Aline Alves Gonçalves <alinealves0@hotmail.com>
2024-02-06 18:00:56 -0300

    Criei o forms.py

---
+++ authors/forms.py
@@ -0,0 +1,14 @@
+from django import forms #type:ignore
+from django.contrib.auth.models import User #type:ignore
+
+
+class RegisterForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = [
+            'first_name',
+            'last_name',
+            'username',
+            'email',
+            'password',
+        ]
