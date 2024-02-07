b482269dcef76c3aad2a68fc6e1da7bea2ad5476
Aline Alves Gonçalves <alinealves0@hotmail.com>
2024-02-07 18:00:01 -0300

    Modificações em forms.py urls.py views.py e register_view.html para o app authors

--- authors/urls.py
+++ authors/urls.py
@@ -2,6 +2,9 @@ from django.urls import path #type:ignore
 
 from . import views
 
+app_name = 'authors'
+
 urlpatterns = [
     path('register/', views.register_view, name='register'),
+    path('register/create/', views.register_create, name='create'),
 ]
