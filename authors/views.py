8b209f4c64abbb5a80ebec5374bb1f480c2491e3
Aline Alves Gonçalves <alinealves0@hotmail.com>
2024-02-29 15:14:09 -0300

    Organizing more files

--- authors/views.py
+++ authors/views.py
@@ -1,14 +1,20 @@
 from django.contrib import messages #type:ignore
 from django.http import Http404 #type:ignore
 from django.shortcuts import redirect, render #type:ignore
+from django.urls import reverse #type:ignore
+
 from .forms import RegisterForm
+
+
 
 def register_view(request):
     register_form_data = request.session.get('register_form_data', None)
     form = RegisterForm(register_form_data)
     return render(request, 'authors/pages/register_view.html', {
         'form': form,
+        'form_action': reverse('authors:create'),
     })
+    
 def register_create(request):
     if not request.POST:
         raise Http404()
